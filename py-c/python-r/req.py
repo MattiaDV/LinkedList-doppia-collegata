0f6cf7dba0ebe1e685a6943e8d09b5605ba6767d
ifthennextagency <ifthen.next.agency@gmail.com>
2026-03-11 09:44:16 +0100

    update linkedlist double

--- py-c/python-r/req.py
+++ py-c/python-r/req.py
@@ -1,14 +1,21 @@
-import requests
+import socket
 import threading
 
-def req():
-    res = requests.get("http://localhost:3000")
-    print(res.status_code)
+IP = "26.18.49.149"
+PORT = 25565
+
+def connect():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((IP, PORT))
+        print("Connesso")
+    except:
+        print("Connessione fallita")
 
 threads = []
-    
-for _ in range(1000):
-    t = threading.Thread(target=req)
+
+for _ in range(200):
+    t = threading.Thread(target=connect)
     t.start()
     threads.append(t)
 
